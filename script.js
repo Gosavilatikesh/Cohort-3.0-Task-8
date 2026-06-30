5dfcbd56acf36c86a353cdcc23d84dd42d67a007
Gosavilatikesh <gosavilatikesh18@gmail.com>
2026-07-01 01:14:41 +0530

    updated css

--- script.js
+++ script.js
@@ -142,3 +142,26 @@ authContainer.style.display = "flex";
 })
 
 // dark mode logic
+const darkToggle = document.querySelector("#darkModeToggle")
+
+darkToggle.addEventListener("change",()=>{
+
+    document.body.classList.toggle("dark");
+
+      localStorage.setItem(
+        "theme",
+        document.body.classList.contains("dark") ? "dark" : "light"
+    );
+     lucide.createIcons();
+
+});
+
+const savedTheme = localStorage.getItem("theme");
+
+if(savedTheme==="dark"){
+
+    document.body.classList.add("dark");
+
+    darkToggle.checked = true;
+
+}
